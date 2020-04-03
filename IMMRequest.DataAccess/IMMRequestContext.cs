a940063db434b626c42057cf24c53dc078f7967b
driden <driden_87@hotmail.com>
2020-04-03 19:02:08 -0300

    moved fields to a folder

--- IMMRequest.DataAccess/IMMRequestContext.cs
+++ IMMRequest.DataAccess/IMMRequestContext.cs
@@ -1,10 +1,30 @@
-﻿using System;
+using IMMRequest.Domain;
+using IMMRequest.Domain.Fields;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace IMMRequest.DataAccess
 {
-    class IMMRequestContext
+    public class IMMRequestContext : DbContext
     {
+        public IMMRequestContext(DbContextOptions<IMMRequestContext> options) : base(options)
+        {
+
+        }
+
+        // Users
+        public DbSet<Admin> Admins { get; set; }
+        public DbSet<Citizen> Citizens { get; set; }
+
+        // Fields
+        public DbSet<Field> Fields{ get; set; }
+
+        // Core
+        public DbSet<RequestArea> RequestAreas { get; set; }
+        public DbSet<RequestTopic> RequestTopics { get; set; }
+        public DbSet<RequestType> RequestTypes { get; set; }
+
     }
 }
