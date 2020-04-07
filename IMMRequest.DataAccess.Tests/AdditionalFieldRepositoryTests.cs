ce547d323bd115643b5d8ae3fce8ec4b81442211
driden <driden_87@hotmail.com>
2020-04-07 19:36:59 -0300

    Another repo test

--- IMMRequest.DataAccess.Tests/AdditionalFieldRepositoryTests.cs
+++ IMMRequest.DataAccess.Tests/AdditionalFieldRepositoryTests.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections.Generic;
 using System.Linq;
 using IMMRequest.DataAccess.Repositories;
 using IMMRequest.Domain.Fields;
@@ -10,7 +12,7 @@ namespace IMMRequest.DataAccess.Tests
     public class AdditionalFieldRepositoryTests
     {
         private IMMRequestContext _context;
-        private AdditionalFieldRepository _repo;
+        private AdditionalFieldRepository _repository;
 
         [TestInitialize]
         public void Setup()
@@ -19,6 +21,7 @@ namespace IMMRequest.DataAccess.Tests
                 new DbContextOptionsBuilder<IMMRequestContext>().UseInMemoryDatabase("AdditionalFields");
             _context = new IMMRequestContext(dbContextOptions.Options);
             _context.Database.EnsureCreated();
+            _repository = new AdditionalFieldRepository(_context);
         }
 
         [TestCleanup]
@@ -30,13 +33,28 @@ namespace IMMRequest.DataAccess.Tests
         [TestMethod]
         public void CanAddAnAdditionalFieldToTheDatabase()
         {
-            _repo.Add(new IntegerField {IsRequired = false, Name = "Int Field", Value = 5});
+            _repository.Add(new IntegerField { IsRequired = false, Name = "Int Field", Value = 5 });
             Assert.AreEqual(1, _context.IntegerFields.Count());
 
             AdditionalField additionalField = _context.IntegerFields.First();
 
             Assert.AreEqual("Int Field", additionalField.Name);
-            Assert.AreEqual(5, ((IntegerField) additionalField).Value);
+            Assert.AreEqual(5, ((IntegerField)additionalField).Value);
+        }
+
+        [TestMethod]
+        public void CannReadAdditionalFieldFromTheDatabase()
+        {
+            var dateField = new DateField
+            {Id = 5, Name = "DateField", Range = new List<Item<DateTime>>(), Value = DateTime.Today };
+
+            _context.DateFields.Add(dateField);
+            _context.SaveChanges();
+
+            var actualField = _repository.Get(5);
+
+            Assert.AreEqual(dateField.Name, actualField.Name);
+            Assert.AreEqual(dateField.Value, actualField.Value);
         }
     }
 }
--- IMMRequest.DataAccess/Repositories/AdditionalFieldRepository.cs
+++ IMMRequest.DataAccess/Repositories/AdditionalFieldRepository.cs
@@ -1,10 +1,24 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using IMMRequest.Domain.Fields;
+using Microsoft.EntityFrameworkCore;
 
 namespace IMMRequest.DataAccess.Repositories
 {
     public class AdditionalFieldRepository
     {
+        public AdditionalFieldRepository(DbContext context)
+        {
+            Context = context;
+        }
+
+        protected DbContext Context { get; set; }
+
+        public virtual void Add(AdditionalField additionalField)
+        {
+            Context.Set<AdditionalField>().Add(additionalField);
+            Context.SaveChanges();
+        }
     }
 }
