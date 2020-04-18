871d3d7a9cca62da753ee931055c217a0b3deaa9
mjaume <57363000+mjaume@users.noreply.github.com>
2020-04-18 16:19:45 -0300

    Added test for domain
    DataFieldTests
    IntegerFieldTests
    TextFieldTests

---
+++ IMMRequest.Domain.Tests/Fields/IntegerFieldTests.cs
@@ -0,0 +1,36 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using IMMRequest.Domain.Fields;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IMMRequest.Domain.Fields.Tests
+{
+    [TestClass()]
+    public class IntegerFieldTests
+    {
+        [TestMethod()]
+        public void AddToRangeTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void IntegerFieldTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ValidateRangeTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void AddToRangeTest1()
+        {
+            Assert.Fail();
+        }
+    }
+}
---
+++ IMMRequest.Domain.Tests/Fields/TextFieldTests.cs
@@ -0,0 +1,36 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using IMMRequest.Domain.Fields;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IMMRequest.Domain.Fields.Tests
+{
+    [TestClass()]
+    public class TextFieldTests
+    {
+        [TestMethod()]
+        public void AddToRangeTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void TextFieldTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ValidateRangeTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void AddToRangeTest1()
+        {
+            Assert.Fail();
+        }
+    }
+}
