440e1482407c21f1793e869c4ce0dd592f3ada8b
driden <lignacio.loureiro@gmail.com>
2020-04-26 13:39:26 -0300

    fixed warnings

--- IMMRequest.Logic.Interfaces/Models/FieldRequestModel.cs
+++ IMMRequest.Logic.Interfaces/Models/FieldRequestModel.cs
@@ -5,7 +5,9 @@ namespace IMMRequest.Logic.Models
         public string Name { get; set; }
         public string Value { get; set; }
 
-        public override bool Equals(object? obj)
+#pragma warning disable 659
+        public override bool Equals(object obj)
+#pragma warning restore 659
         {
             return obj is FieldRequestModel frm && frm.Name == Name && frm.Value == Value;
         }
