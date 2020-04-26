4d5c88f8c7b0f4556623376ef1e74b1d84e6a5ec
driden <lignacio.loureiro@gmail.com>
2020-04-26 12:36:52 -0300

    moved and renamed FieldRequest to FieldRequestModel.cs

---
+++ IMMRequest.Logic.Interfaces/Models/FieldRequestModel.cs
@@ -0,0 +1,13 @@
+namespace IMMRequest.Logic.Models
+{
+    public class FieldRequestModel
+    {
+        public string Name { get; set; }
+        public string Value { get; set; }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is FieldRequestModel fr && fr.Name == Name && fr.Value == Value;
+        }
+    }
+}
