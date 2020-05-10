c5b05da994bef75d0d3675ebd0b28926d43f015e
driden <lignacio.loureiro@gmail.com>
2020-05-10 19:36:19 -0300

    Fixed the bug where crating a new Request would create a new user

--- Codigo/IMMRequest.DataAccess.Core/Repositories/RequestRepository.cs
+++ Codigo/IMMRequest.DataAccess.Core/Repositories/RequestRepository.cs
@@ -1,10 +1,24 @@
 namespace IMMRequest.DataAccess.Core.Repositories
 {
+    using System.Linq;
     using Domain;
     using Microsoft.EntityFrameworkCore;
 
     public class RequestRepository : Repository<Request>
     {
         public RequestRepository(DbContext context) : base(context) { }
+
+        public override void Add(Request entity)
+        {
+            var user = Context.Set<Citizen>().FirstOrDefault(u => u.Email == entity.Citizen.Email);
+            if (user != null)
+            {
+                entity.Citizen = user;
+            }
+
+            Context.Set<Request>().Add(entity);
+            Context.SaveChanges();
+
+        }
     }
 }
