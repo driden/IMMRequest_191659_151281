1aa5a4968ac4b0d1bbadf5cbfb0c68df8189e4a6
driden <lignacio.loureiro@gmail.com>
2020-06-23 18:46:26 -0300

    test fix

--- Codigo/IMMRequest.JsonRequestImporter/IMMRequest.JsonRequestImporter.Tests/ImportTests.cs
+++ Codigo/IMMRequest.JsonRequestImporter/IMMRequest.JsonRequestImporter.Tests/ImportTests.cs
@@ -18,7 +18,9 @@ namespace IMMRequest.JsonRequestImporter.Tests
         public void ItCanParseRequests()
         {
             IRequestsImportable importer = new JsonRequestImporter();
-            var list = importer.Import(@"requests.json");
+            var content =
+                "{\r\n  \"requests\": [\r\n    {\r\n      \"details\": \"some details\",\r\n      \"email\": \"someemail@citizen.com\",\r\n      \"name\": \"some citizens name\",\r\n      \"phone\": \"5555-555-555\",\r\n      \"typeId\": 1,\r\n      \"additionalFields\": [\r\n        {\r\n          \"name\": \"Nro de Movil\",\r\n          \"values\": \"111111|222222\"\r\n        },\r\n        {\r\n          \"name\": \"Fecha y hora\",\r\n          \"values\": \"9/5/2020|10/6/2021\"\r\n        }\r\n      ]\r\n    }\r\n  ]\r\n}\r\n";
+            var list = importer.Import(content);
             Assert.AreEqual(1,list.Requests.Count);
         }
     }
